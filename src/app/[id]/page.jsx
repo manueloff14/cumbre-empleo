8720ba5778531bcd5d869d2b0baa24472e1b8f8a
manueloff14 <manueloffcabrera@gmail.com>
2024-10-29 14:23:19 -0500

    class actualizados por className

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -83,7 +83,7 @@ export default function Page() {
             {!loading && data ? (
               <JobData data={data} jobDetails={jobDetails} />
             ) : (
-              <p>Cargando datos del trabajo...</p>
+              <p>Cargando datos de la vacante...</p>
             )}
           </div>
         </div>
