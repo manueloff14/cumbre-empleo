b331be1b3b7bd8a74274d5873bdfe595ca0c3b09
manueloff14 <manueloffcabrera@gmail.com>
2024-11-17 16:42:15 -0500

    cambios en el tag y head corregido

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,11 +15,30 @@ const geistMono = localFont({
 export const metadata = {
   title: "Cargando la vacante",
   description: "Cargando la vacante",
+  viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        {/* Incluir script externo */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D54LGRR265"
+        ></script>
+        {/* Incluir script inline */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-D54LGRR265');
+            `,
+          }}
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
