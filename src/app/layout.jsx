764acd4deb452a7a1554f01befd0595c65b5d56b
Manuel Cabrera <107600099+manueloff14@users.noreply.github.com>
2024-11-28 13:59:12 -0500

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
             `,
           }}
         ></script>
-	<meta name="monetag" content="0e36d4ac78c415595124f07b72e2faa3">
+	<meta name="monetag" content="0e36d4ac78c415595124f07b72e2faa3" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
