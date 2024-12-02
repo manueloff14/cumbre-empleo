9a5ca5980d35ef9fbf98a0f612ff4c52f1da1469
Manuel Cabrera <107600099+manueloff14@users.noreply.github.com>
2024-12-02 08:58:43 -0500

    Update JobDescription.jsx

--- components/JobDescription.jsx
+++ components/JobDescription.jsx
@@ -2,8 +2,20 @@ import ReactMarkdown from 'react-markdown';
 
 export default function JobDescription({ jobDescription }) {
     return (
-        <div className="prose max-w-none dark:prose-invert">
-            <ReactMarkdown>{jobDescription}</ReactMarkdown>
-        </div>
+        <>
+            <script type="text/javascript">
+            	atOptions = {
+            		'key' : '81de62cf173c21bf4c00f1c25d789f71',
+            		'format' : 'iframe',
+            		'height' : 90,
+            		'width' : 728,
+            		'params' : {}
+            	};
+            </script>
+            <script type="text/javascript" src="//www.highperformanceformat.com/81de62cf173c21bf4c00f1c25d789f71/invoke.js"></script>
+            <div className="prose max-w-none dark:prose-invert">
+                <ReactMarkdown>{jobDescription}</ReactMarkdown>
+            </div>
+        </>
     );
 }
