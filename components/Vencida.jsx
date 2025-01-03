8a884732949533701f2b960a86b503ec5226251a
manueloff14 <manueloffcabrera@gmail.com>
2025-01-03 11:48:20 -0500

    corregimos cosas en ui del Vencida.jsx

--- components/Vencida.jsx
+++ components/Vencida.jsx
@@ -16,14 +16,17 @@ const Vencida = () => {
     return (
         <>
             {/* Fondo semitransparente */}
-            <div className="fixed inset-0 bg-white dark:bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="fixed inset-0 bg-white dark:bg-black bg-opacity-50 dark:bg-opacity-50 flex items-center justify-center z-50 transition-opacity duration-300 ease-in-out">
                 {/* Contenedor del Modal */}
-                <div className="bg-gradient-to-br from-red-400 to-red-600 border-2 border-red-500 dark:border-red-700 rounded-2xl p-6 text-center shadow-xl w-[80%] sm:w-[50%] lg:w-[30%] ">
-                    <h3 className="text-xl font-bold text-white">
+                <div className="bg-gradient-to-br from-red-400 to-red-600 border-2 border-red-500 dark:border-red-700 rounded-2xl p-6 w-[80%] text-center shadow-xl transform transition-transform duration-300 ease-in-out scale-100 sm:w-[50%] lg:w-[30%]">
+                    <h3 className="text-2xl font-bold text-white">
                         ¡Vacante Vencida!
                     </h3>
                     <p className="mt-2 text-white">
                         Esta vacante ya no está disponible.
+                    </p>
+                    <p className="mt-2 text-white font-bold">
+                        Te recomendamos usar el búscador.
                     </p>
                 </div>
             </div>
