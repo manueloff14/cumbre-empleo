a1dc0fea87bd9b3e85bcbf58afe04fb118ab7490
manueloff14 <manueloffcabrera@gmail.com>
2025-01-02 20:52:56 -0500

    quitamos el banner

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -49,7 +49,7 @@ export default function RootLayout({ children }) {
       >
         {children}
 
-        <LayoutBanner />
+        {/* <LayoutBanner /> */}
 
         {/* <ClickPopupAdvanced /> */}
       </body>
